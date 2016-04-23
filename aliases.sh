fd584013173f4d0d0805098be92f933fcf60b3c4
Matthew Bussa <matthewbussa@gmail.com>
2016-04-23 14:45:03 -0400

    beginnings of rails aliases

--- aliases.sh
+++ aliases.sh
@@ -11,8 +11,14 @@ alias reboot='sudo reboot'
 #Networking
 alias lip='ifconfig | grep 192'
 alias server='python -m SimpleHTTPServer'
+
 #Docker
 alias dm='docker-machine'
 
 # Git Aliases
 alias st='git status'
+
+# Rails Aliases
+alias be='bundle exec'
+alias rs='be rails server'
+alias startps='postgres -D /usr/local/var/postgres'
