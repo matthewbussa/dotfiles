d23df7ed1afe711bce11728c52a24fbace74d58a
Matthew Bussa <matthewbussa@gmail.com>
2016-06-15 10:22:47 -0400

    added aliases for glog and lock

--- .profile
+++ .profile
@@ -7,9 +7,14 @@ export NVM_DIR="/usr/local/opt/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 source ~/dotfiles/.git-completion.bash
+source ~/dotfiles/.git-prompt.bash
 
 source ~/dotfiles/.docker-completion.bash
 source ~/dotfiles/.docker-machine-completion.bash
 source ~/dotfiles/.docker-compose-completion.bash
 
 source ~/dotfiles/aliases.sh
+
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+    . $(brew --prefix)/etc/bash_completion
+fi
--- aliases.sh
+++ aliases.sh
@@ -7,6 +7,7 @@ alias pp='cd ~/PersonalProjects'
 
 #Sytem Related
 alias reboot='sudo reboot'
+alias lock='/System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Resources/CGSession -suspend'
 
 #Networking
 alias lip='ifconfig | grep 192'
@@ -17,6 +18,7 @@ alias dm='docker-machine'
 
 # Git Aliases
 alias st='git status'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 # Rails Aliases
 alias be='bundle exec'
